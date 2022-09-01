22dca4bf61e357a031c0a5d861d03df1835ea54c
Hiranmoy Mandal <94227277+Hiranmoy45@users.noreply.github.com>
2022-09-01 15:36:45 +0530

    update

---
+++ lec4/pattern1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=0,j=1,n;
+    cout<< "Enter the value of n:";
+    cin>> n;
+    while(i<n){
+        while(j<=n){
+            cout << j ;
+        }
+        cout << "\n";
+    }
+}
